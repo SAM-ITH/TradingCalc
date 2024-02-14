7a04dcb0ad2055ce22003c4b5e1458a6f9025db2
Samith <samithwijesinha@gmail.com>
2024-02-14 07:53:14 +0530

    adding the project files

---
+++ tradingCalc/tradingCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  tradingCalcApp.swift
+//  tradingCalc
+//
+//  Created by Samith on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct tradingCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
