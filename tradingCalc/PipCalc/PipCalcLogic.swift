096c82eb170dd737ebb8e3fec561f668fef7bb2e
Samith <samithwijesinha@gmail.com>
2024-02-19 08:39:45 +0530

    basic UI changes and add pipCalacLogic file

---
+++ tradingCalc/PipCalc/PipCalcLogic.swift
@@ -0,0 +1,8 @@
+//
+//  PipCalcLogic.swift
+//  tradingCalc
+//
+//  Created by Samith on 2024-02-15.
+//
+
+import Foundation
--- tradingCalc/PipCalc/Views/PipCalcView.swift
+++ tradingCalc/PipCalc/Views/PipCalcView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct PipCalcView: View {
+    @State private var baseCurrency = "USD"
+    let baseCurrencyItems = ["USD", "EUR", "GBP"]
+    @State private var currencyPair = "USD/EUR"
+    let currencyPairItems = ["USD/EUR", "GBP/USD", "GBP/JPY"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, trading calc!")
+        VStack{
+            Text("Trade")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(20)
+            /// base currency selection
+            HStack(spacing: 60){
+                Text("Base currency")
+                    .font(.headline)
+                Picker("Choose", selection: $baseCurrency) {
+                    ForEach(baseCurrencyItems, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            /// instrument pair selection
+            HStack(spacing: 60){
+                Text("Currency pair")
+                    .font(.headline)
+                Picker("Choose", selection: $currencyPair) {
+                    ForEach(currencyPairItems, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            /// trade size selection
+            HStack(spacing: 60){
+                Text("Currency pair")
+                    .font(.headline)
+                Picker("Choose", selection: $currencyPair) {
+                    ForEach(currencyPairItems, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            /// showing the calculated value
+            Text("Pip value")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(20)
+            /// showing the calculated pip value
+            HStack(spacing: 60){
+                Text("Pip value")
+                    .font(.headline)
+                
+            }
         }
-        .padding()
     }
 }
 
