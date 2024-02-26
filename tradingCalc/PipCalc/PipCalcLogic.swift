b5d145e1c624b48d21957ef0966bd97813de945f
Samith <samithwijesinha@gmail.com>
2024-02-26 23:57:55 +0530

    add PipCalcLogic class to calculate pip value in account base currency

--- tradingCalc/PipCalc/PipCalcLogic.swift
+++ tradingCalc/PipCalc/PipCalcLogic.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+/// formula for calculating the pip value
+/// pip value = [lot size]*[trade size]
+///             ------------------------
+///             [current market price]
+/// pip value ---> converted to account base cuurency
+
+class PipCalcLogic {
+    func baseIsUsd(lotSize:Double, tradeSize:Double, currentMaketPrice:Double) -> Double {
+        return (lotSize * tradeSize)/currentMaketPrice
+    }
+}
